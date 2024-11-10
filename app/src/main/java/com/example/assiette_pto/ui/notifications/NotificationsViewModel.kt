89d1b999fefa9c17cf07f67cc6dd5d0e8fa49af1
Anfal Chaiba <78087375+ChaibaAnfal@users.noreply.github.com>
2024-11-10 13:34:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/assiette_pto/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.assiette_pto.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
