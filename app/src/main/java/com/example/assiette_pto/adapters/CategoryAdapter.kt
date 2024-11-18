abb29b50125b01a223edb891c9d17efd84f7d639
Kamar <kamardoblibennani@gmail.com>
2024-11-18 23:32:50 +0100

    menu grid
    arrow working for navigation

--- app/src/main/java/com/example/assiette_pto/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/assiette_pto/adapters/CategoryAdapter.kt
@@ -6,18 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.assiette_pto.R
 import com.example.assiette_pto.responses.Category
 import com.squareup.picasso.Picasso
 
-class CategoryAdapter(private var categories: List<Category>,
-                      private val onCategoryClick: (String) -> Unit) :
-    RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+class CategoryAdapter(
+    private var categories: List<Category>,
+    private val onCategoryClick: (String) -> Unit
+) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
 
     class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val categoryName: TextView = itemView.findViewById(R.id.tvCategoryName)
         val categoryImage: ImageView = itemView.findViewById(R.id.ivCategoryImage)
+        val categoryCard: CardView = itemView.findViewById(R.id.cardCategory)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
@@ -29,9 +32,8 @@ class CategoryAdapter(private var categories: List<Category>,
         val category = categories[position]
         holder.categoryName.text = category.name
         Picasso.get().load(category.thumbnail).into(holder.categoryImage)
-
-        holder.itemView.setOnClickListener {
-            onCategoryClick(category.name) // Pass the category name to the click listener
+        holder.categoryCard.setOnClickListener {
+            onCategoryClick(category.name)
         }
     }
 
